bd882eefee4747e6fc2cf833417b6e6e3c1d60e3
lovehifi <81028393+lovehifi@users.noreply.github.com>
2023-10-11 22:33:41 +0700

    Update sq.sh

--- sq.sh
+++ sq.sh
@@ -4,5 +4,5 @@
 card=$(grep -oP '(?<=defaults.pcm.card ).*' /etc/asound.conf)
 echo "Card number: $card"
 
-/opt/sq/squeezelite64 -o hw:$card -n SQ64-rAudio -s 127.0.0.1 -m 00:00:00:00:00:00 -a 4096:4
+/opt/sq/squeezelite64 -o hw:$card -n SQ64-rAudio -s 127.0.0.1 -m 00:00:00:00:00:00 -W
 
