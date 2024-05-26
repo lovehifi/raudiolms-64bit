09efa36bf3af834852bbdb99a196e5101bad3f75
lovehifi <81028393+lovehifi@users.noreply.github.com>
2024-05-26 16:44:43 +0700

    Update install-8.5.sh

--- install-8.5.sh
+++ install-8.5.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-sudo rm -f /var/lib/pacman/sync/* && sudo pacman -Syu --noconfirm
 (getent group logitechms &>/dev/null || sudo groupadd -r logitechms >/dev/null) && (getent passwd logitechms &>/dev/null || sudo useradd -r -g logitechms -d /opt/logitechmediaserver -c 'Logitech Media Server' -s /bin/false logitechms >/dev/null) && sudo mkdir -p /opt/logitechmediaserver/{cache,Logs,prefs{,/plugin},Plugins} && sudo touch /opt/logitechmediaserver/Logs/slimserver.log && sudo chown -R logitechms:logitechms /opt/logitechmediaserver && echo 'logitechms ALL=(ALL) NOPASSWD: ALL' | sudo tee -a /etc/sudoers
+
+sudo rm -f /var/lib/pacman/sync/* && pacman -Syy --noconfirm && sudo pacman -Syu --noconfirm
+
 cd /tmp && wget https://raw.githubusercontent.com/lovehifi/raudiolms-64bit/main/logitechmediaserver-8.5.1-2-aarch64.pkg.tar.xz && pacman -U --noconfirm /tmp/logitechmediaserver-8.5.1-2-aarch64.pkg.tar.xz
+
 sudo pacman -S --noconfirm perl perl-cgi perl-dbi perl-digest-sha1 perl-html-parser perl-json perl-libwww perl-lwp-protocol-https perl-net-ssleay perl-test-pod perl-test-warn perl-xml-parser
+
 sudo systemctl daemon-reload && sudo systemctl enable logitechmediaserver.service && sudo systemctl restart logitechmediaserver.service && sudo systemctl status logitechmediaserver.service
