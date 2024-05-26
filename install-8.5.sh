be2d2c220d80c533ea0e21c66857cd4a33ac4726
lovehifi <81028393+lovehifi@users.noreply.github.com>
2024-05-26 13:22:30 +0700

    Update install-8.5.sh

--- install-8.5.sh
+++ install-8.5.sh
@@ -14,3 +14,5 @@ fi
 (getent group logitechms &>/dev/null || sudo groupadd -r logitechms >/dev/null) && (getent passwd logitechms &>/dev/null || sudo useradd -r -g logitechms -d /opt/logitechmediaserver -c 'Logitech Media Server' -s /bin/false logitechms >/dev/null) && sudo mkdir -p /opt/logitechmediaserver/{cache,Logs,prefs{,/plugin},Plugins} && sudo touch /opt/logitechmediaserver/Logs/slimserver.log && sudo chown -R logitechms:logitechms /opt/logitechmediaserver && echo 'logitechms ALL=(ALL) NOPASSWD: ALL' | sudo tee -a /etc/sudoers
 
 cd /tmp && wget https://raw.githubusercontent.com/lovehifi/raudiolms-64bit/main/logitechmediaserver-8.5.1-2-aarch64.pkg.tar.xz && pacman -U --noconfirm /tmp/logitechmediaserver-8.5.1-2-aarch64.pkg.tar.xz
+
+sudo systemctl daemon-reload && sudo systemctl enable logitechmediaserver-git.service && sudo systemctl restart logitechmediaserver-git.service
