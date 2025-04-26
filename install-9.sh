51d8101a4b0c61e2dee1779464dd2bc2f9872070
lovehifi <81028393+lovehifi@users.noreply.github.com>
2025-04-26 19:25:46 +0700

    Update install-9.sh

--- install-9.sh
+++ install-9.sh
@@ -12,8 +12,32 @@ sudo pacman -S perl-xml-parser --noconfirm
 sudo pacman -S perl-html-parser --noconfirm
 sudo pacman -S perl-app-cpanminus --noconfirm
 cd /tmp
-tar xzvf /tmp/lyrionmusicserver.tar.gz -C /tmp/ --overwrite
-mkdir -p /home/pi
-mv /tmp/home/pi/lyrionmusicserver /home/pi/
+sudo tar xzvf /tmp/lyrionmusicserver.tar.gz -C /tmp/ --overwrite
+sudo mkdir -p /home/pi
+sudo mv /tmp/home/pi/lyrionmusicserver /home/pi/
+
+sudo bash -c 'cat > /etc/systemd/system/lyrionmusicserver.service << "EOF"
+[Unit]
+Description=Lyrion Music Server
+After=sound.target squeezelite.service
+
+[Service]
+User=alarm
+Group=alarm
+WorkingDirectory=/home/pi/lyrionmusicserver
+ExecStart=/usr/bin/perl /home/pi/lyrionmusicserver/slimserver.pl \
+  --prefsdir /home/pi/lyrionmusicserver/prefs \
+  --cachedir /home/pi/lyrionmusicserver/cache \
+  --logdir   /home/pi/lyrionmusicserver/Logs
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF'
+
+sudo chown alarm:alarm -Rv /home/pi/lyrionmusicserver
+sudo systemctl daemon-reexec
+sudo systemctl daemon-reload
+sudo systemctl enable --now lyrionmusicserver
 
 
