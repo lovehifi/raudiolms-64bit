db7f26b3ba8f364cad33fefea350602cc32b3689
lovehifi <81028393+lovehifi@users.noreply.github.com>
2025-04-26 19:19:13 +0700

    Update install-9.sh

--- install-9.sh
+++ install-9.sh
@@ -11,6 +11,9 @@ sudo pacman -S perl-io-socket-ssl --noconfirm
 sudo pacman -S perl-xml-parser --noconfirm
 sudo pacman -S perl-html-parser --noconfirm
 sudo pacman -S perl-app-cpanminus --noconfirm
-cd /tmp && wget https://raw.githubusercontent.com/lovehifi/raudiolms-64bit/main/lyrionmusicserver-9.1.0-1743832628-backup.tar.gz
-tar xzvf /tmp/lyrionmusicserver-9.1.0-1743832628-backup.tar.gz -C ~ --overwrite
+cd /tmp
+tar xzvf /tmp/lyrionmusicserver.tar.gz -C /tmp/ --overwrite
+mkdir -p /home/pi
+mv /tmp/home/pi/lyrionmusicserver /home/pi/
+
 
